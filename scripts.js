dc68fa123cc0e6c14a42c35e439ced10d1d40639
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-02-01 22:58:05 -0300

    random deal

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-/*
-<li class="card" data-identifier="card">
-    <div class="back-side" data-identifier="back-face">
-        <img src="media//front.png" alt="back-side parrot">
-    </div>
-    <div class="front-side hidden" data-identifier="front-face"></div>
-</li>
-*/
-
 function askNumber() {
     let numberOfCards;
     do {
@@ -17,25 +8,47 @@ function askNumber() {
 }
 
 function dealCards() {
+    const parrotLibrary = [
+        'media/explodyparrot.gif', 'media/explodyparrot.gif',
+        'media/revertitparrot.gif', 'media/revertitparrot.gif',
+        'media/tripletsparrot.gif', 'media/tripletsparrot.gif',
+        'media/metalparrot.gif', 'media/metalparrot.gif',
+        'media/fiestaparrot.gif', 'media/fiestaparrot.gif',
+        'media/bobrossparrot.gif', 'media/bobrossparrot.gif',
+        'media/unicornparrot.gif', 'media/unicornparrot.gif'
+        ];
     const deckOfCards = [];
     const num = askNumber();
 
     for (let i=0; i<num; i++) {
         deckOfCards.push(`
-        <li class="card">
-            <div class="front-side">
-                <img src="media//front.png" alt="back-side parrot">
+        <li class="card" data-identifier="card" onclick="turnCard(this)">
+            <div class="back-side" data-identifier="back-face">
+                <img src="media/front.png" alt="back-side parrot">
             </div>
-            <div class="back-side hidden"></div>
+            <div class="front-side hidden" data-identifier="front-face">
+                <img src=${parrotLibrary[i]}>
+            </div>
         </li>
         `);
     }
 
     const deck = document.querySelector('.deck');
-    console.log(deck);
+    deckOfCards.sort(comparador);
     for (let i=0; i<deckOfCards.length; i++) {
         deck.innerHTML += deckOfCards[i];
     }
+}
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+function turnCard(card) {
+    const back = card.querySelector('.back-side');
+    const front = card.querySelector('.front-side');
+    back.classList.add('hidden');
+    front.classList.remove('hidden');
 }
 
 dealCards();
